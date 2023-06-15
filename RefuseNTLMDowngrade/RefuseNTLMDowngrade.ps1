773710075a6ca62cd6e1b5d46a947706995070d4
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-15 13:52:58 +0000

    Update RefuseNTLMDowngrade.ps1

--- RefuseNTLMDowngrade/RefuseNTLMDowngrade.ps1
+++ RefuseNTLMDowngrade/RefuseNTLMDowngrade.ps1
@@ -2,14 +2,14 @@
 $regKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
 $key1 = "LMCompatibilityLevel"
 
-# Check if the value exists and is already set to 1
+# Check if the value exists and is already set to 5
 if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key1 -ErrorAction SilentlyContinue).$key1 -eq 5) {
     Write-Host "The value of '$key1' is already set to 5." ##5 is highest level
 
 }
 else {
 try {
-    # Set the value to 1
+    # Set the value to 5
     Set-ItemProperty -Path $regKey -Name $key1 -Value 5 -Type DWORD -Force
     Write-Host "The value of '$key1' has been set to 5."
 }
