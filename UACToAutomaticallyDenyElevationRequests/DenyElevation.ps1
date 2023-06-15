6b342d575677b88530c18692efa2942d180b02de
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-15 14:37:59 +0000

    Create DenyElevation.ps1

---
+++ UACToAutomaticallyDenyElevationRequests/DenyElevation.ps1
@@ -0,0 +1,20 @@
+# Define the registry key and value name
+$regKey= "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$key0 = "ConsentPromptBehaviourUser"
+
+# Check if the value exists and is already set to 0
+if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key0 -ErrorAction SilentlyContinue).$key0 -eq 0) {
+    Write-Host "The value of '$key0' is already set to 0."
+
+}
+else {
+try {
+    # Set the value
+    Set-ItemProperty -Path $regKey -Name $key0 -Value 0 -Type DWORD -Force
+    Write-Host "The value of '$key0' has been set to 0."
+}
+
+catch {
+    Exit 0
+}
+}
