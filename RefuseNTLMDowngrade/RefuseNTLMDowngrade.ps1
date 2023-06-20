9e24d094dfbbcebcde2e240c8b284e2b67ad480c
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 06:58:50 +0000

    Update RefuseNTLMDowngrade.ps1

--- RefuseNTLMDowngrade/RefuseNTLMDowngrade.ps1
+++ RefuseNTLMDowngrade/RefuseNTLMDowngrade.ps1
@@ -4,14 +4,14 @@ $key1 = "LMCompatibilityLevel"
 
 # Check if the value exists and is already set to 5
 if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key1 -ErrorAction SilentlyContinue).$key1 -eq 5) {
-    Write-Host "The value of '$key1' is already set to 5." ##5 is highest level
+    Write-Host "The value of '$key1' under $regKey is already set to 5." ##5 is highest level
 
 }
 else {
 try {
     # Set the value to 5
     Set-ItemProperty -Path $regKey -Name $key1 -Value 5 -Type DWORD -Force
-    Write-Host "The value of '$key1' has been set to 5."
+    Write-Host "The value of '$key1' under $regKey has been set to 5."
 }
 
 catch {
