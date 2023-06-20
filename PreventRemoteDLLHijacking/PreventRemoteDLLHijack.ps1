c2636bffeb997c0ab4442dfcc38135451502c1df
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 07:20:40 +0000

    Create PreventRemoteDLLHijack.ps1

---
+++ PreventRemoteDLLHijacking/PreventRemoteDLLHijack.ps1
@@ -0,0 +1,3 @@
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v CWDIllegalInDllSearch /t REG_DWORD /d 0x2 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v SafeDLLSearchMode /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v ProtectionMode /t REG_DWORD /d 1 /f
