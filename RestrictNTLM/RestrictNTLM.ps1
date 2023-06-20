943e9b81a71d46ef5ba336661d4d13c894c5c8b9
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 07:13:47 +0000

    Create RestrictNTLM.ps1

---
+++ RestrictNTLM/RestrictNTLM.ps1
@@ -0,0 +1,2 @@
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa\MSV1_0" /v RestrictReceivingNTLMTraffic /t REG_DWORD /d 2 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa\MSV1_0" /v RestrictSendingNTLMTraffic /t REG_DWORD /d 2 /f
