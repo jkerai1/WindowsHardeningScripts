15a24ae1c6b553eaf7d27cae75738c9d94d2ab48
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 06:56:13 +0000

    Update DisableLocalDomainCreds.ps1

--- DisableLocalStorageOfCreds/DisableLocalDomainCreds.ps1
+++ DisableLocalStorageOfCreds/DisableLocalDomainCreds.ps1
@@ -4,14 +4,14 @@ $key0 = "DisableDomainCreds"
 
 # Check if the value exists and is already set to 1
 if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key0 -ErrorAction SilentlyContinue).$key0 -eq 1) {
-    Write-Host "The value of '$key0' is already set to 1."
+    Write-Host "The value of '$key0' under $regKey is already set to 1."
 
 }
 else {
 try {
     # Set the value
     Set-ItemProperty -Path $regKey -Name $key0 -Value 1 -Type DWORD -Force
-    Write-Host "The value of '$key0' has been set to 1."
+    Write-Host "The value of '$key0' under $regKey has been set to 1."
 }
 
 catch {
