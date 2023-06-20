4975921f501974a55d17b475ec30796d252aa929
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 07:31:37 +0000

    Create EnableFWLogging.ps1

---
+++ WindowsFirewallHardening/EnableFWLogging.ps1
@@ -0,0 +1,3 @@
+netsh advfirewall set currentprofile logging filename %systemroot%\system32\LogFiles\Firewall\pfirewall.log
+netsh advfirewall set currentprofile logging maxfilesize 4096
+netsh advfirewall set currentprofile logging droppedconnections enable
