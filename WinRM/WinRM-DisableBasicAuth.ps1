67c84f46b4f7db3e010619c691fc72135ee54a36
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 06:59:45 +0000

    Update WinRM-DisableBasicAuth.ps1

--- WinRM/WinRM-DisableBasicAuth.ps1
+++ WinRM/WinRM-DisableBasicAuth.ps1
@@ -5,7 +5,7 @@ $key0 = "AllowBasic"
 
 # Check if the value exists and is already set to 0
 if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key0 -ErrorAction SilentlyContinue).$key0 -eq 0) {
-    Write-Host "The value of '$key0' is already set to 0."
+    Write-Host "The value of '$key0' under $regKey is already set to 0."
 
 }
 else {
@@ -14,7 +14,7 @@ try {
     New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\WinRM"
     New-Item -Path $regkey -Force
     Set-ItemProperty -Path $regKey -Name $key0 -Value 0 -Type DWORD -Force
-    Write-Host "The value of '$key0' has been set to 0."
+    Write-Host "The value of '$key0' under $regKey has been set to 0."
 }
 
 catch {
@@ -23,7 +23,7 @@ catch {
 }
 
 if ((Test-Path $regKey1) -and (Get-ItemProperty $regKey -Name $key0 -ErrorAction SilentlyContinue).$key0 -eq 0) {
-    Write-Host "The value of '$key0' is already set to 0."
+    Write-Host "The value of '$key0' under $regKey1 is already set to 0."
 
 }
 
@@ -33,7 +33,7 @@ try {
     ##New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\WinRM"
     New-Item -Path $regkey1 -Force
     Set-ItemProperty -Path $regKey1 -Name $key0 -Value 0 -Type DWORD -Force
-    Write-Host "The value of '$key0' has been set to 0."
+    Write-Host "The value of '$key0' under $regKey1 has been set to 0."
 }
 
 catch {
