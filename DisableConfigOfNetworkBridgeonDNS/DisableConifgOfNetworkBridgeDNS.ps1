ad047547bd9c3689fec995296cf9394ee12f7ae4
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 06:55:51 +0000

    Update DisableConifgOfNetworkBridgeDNS.ps1

--- DisableConfigOfNetworkBridgeonDNS/DisableConifgOfNetworkBridgeDNS.ps1
+++ DisableConfigOfNetworkBridgeonDNS/DisableConifgOfNetworkBridgeDNS.ps1
@@ -4,14 +4,14 @@ $key0 = "NC_AllowNetBridge_NLA"
 
 # Check if the value exists and is already set to 0
 if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key0 -ErrorAction SilentlyContinue).$key0 -eq 0) {
-    Write-Host "The value of '$key0' is already set to 0."
+    Write-Host "The value of '$key0' under $regKey is already set to 0."
 
 }
 else {
 try {
     # Set the value
     Set-ItemProperty -Path $regKey -Name $key0 -Value 0 -Type DWORD -Force
-    Write-Host "The value of '$key0' has been set to 0."
+    Write-Host "The value of '$key0' under $regKey has been set to 0."
 }
 
 catch {
