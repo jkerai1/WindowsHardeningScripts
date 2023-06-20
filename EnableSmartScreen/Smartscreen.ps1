8cf9cd4d7091ac81e3cc27a81297866943940324
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 07:18:56 +0000

    Create Smartscreen.ps1

---
+++ EnableSmartScreen/Smartscreen.ps1
@@ -0,0 +1,2 @@
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableSmartScreen /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v ShellSmartScreenLevel /t REG_SZ /d Block /f
