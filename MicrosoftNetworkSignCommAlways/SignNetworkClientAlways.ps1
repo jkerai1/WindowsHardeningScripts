2c1a9f077862857dfaaa5c902ca7cf67fb33865d
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 06:58:17 +0000

    Update SignNetworkClientAlways.ps1

--- MicrosoftNetworkSignCommAlways/SignNetworkClientAlways.ps1
+++ MicrosoftNetworkSignCommAlways/SignNetworkClientAlways.ps1
@@ -5,14 +5,14 @@ $key1 = "EnableSecuritySignature"
 
 # Check if the value exists and is already set to 1
 if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key0 -ErrorAction SilentlyContinue).$key0 -eq 1) {
-    Write-Host "The value of '$key0' is already set to 1."
+    Write-Host "The value of '$key0' under $regKey is already set to 1."
 
 }
 else {
 try {
     # Set the value
     Set-ItemProperty -Path $regKey -Name $key0 -Value 1 -Type DWORD -Force
-    Write-Host "The value of '$key0' has been set to 1."
+    Write-Host "The value of '$key0' under $regKey has been set to 1."
 }
 
 catch {
@@ -21,14 +21,14 @@ catch {
 }
 
 if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key1 -ErrorAction SilentlyContinue).$key1 -eq 1) {
-    Write-Host "The value of '$key1' is already set to 1."
+    Write-Host "The value of '$key1' under $regKey is already set to 1."
 
 }
 else {
 try {
     # Set the value
     Set-ItemProperty -Path $regKey -Name $key1 -Value 1 -Type DWORD -Force
-    Write-Host "The value of '$key1' has been set to 1."
+    Write-Host "The value of '$key1' under $regKey has been set to 1."
 }
 
 catch {
