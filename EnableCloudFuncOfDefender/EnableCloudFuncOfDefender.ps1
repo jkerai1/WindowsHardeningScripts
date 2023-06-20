c2ebe8bc84cdcb84ad440c8ac10c35a9d36d6728
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 07:07:48 +0000

    Update EnableCloudFuncOfDefender.ps1

--- EnableCloudFuncOfDefender/EnableCloudFuncOfDefender.ps1
+++ EnableCloudFuncOfDefender/EnableCloudFuncOfDefender.ps1
@@ -1,3 +1,5 @@
 #Enable Cloud functionality of Windows Defender
 Set-MpPreference -MAPSReporting 2
 Set-MpPreference -SubmitSamplesConsent 3
+Set-MpPreference -CloudBlockLevel ZeroTolerance
+Set-MpPreference -CloudExtendedTimeout 50
