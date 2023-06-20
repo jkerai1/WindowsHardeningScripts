dbe15d28ddb89bd97afa604cb82503d2bc9ad0e8
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 06:56:49 +0000

    Update DisableSolictedRemoteAssistance.ps1

--- DisableSolictedRemoteAssistance/DisableSolictedRemoteAssistance.ps1
+++ DisableSolictedRemoteAssistance/DisableSolictedRemoteAssistance.ps1
@@ -4,7 +4,7 @@ $key0 = "fAllowToGetHelp"
 
 # Check if the value exists and is already set to 0
 if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key0 -ErrorAction SilentlyContinue).$key0 -eq 0) {
-    Write-Host "The value of '$key0' is already set to 0."
+    Write-Host "The value of '$key0' under $regKey is already set to 0."
 
 }
 else {
@@ -12,7 +12,7 @@ try {
     # Set the value
     #New-Item -Path $regKey
     Set-ItemProperty -Path $regKey -Name $key0 -Value 0 -Type DWORD -Force
-    Write-Host "The value of '$key0' has been set to 0."
+    Write-Host "The value of '$key0' under $regKey has been set to 0."
 }
 
 catch {
