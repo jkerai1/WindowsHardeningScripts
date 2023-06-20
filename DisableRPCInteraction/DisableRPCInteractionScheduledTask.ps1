5ae6fb9fd2662c86c8d0ea41c11cd6b8675e77cf
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 07:27:37 +0000

    Create DisableRPCInteractionScheduledTask.ps1

---
+++ DisableRPCInteraction/DisableRPCInteractionScheduledTask.ps1
@@ -0,0 +1,3 @@
+#Disabling RPC usage from a remote asset interacting with scheduled tasks
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Schedule" /v DisableRpcOverTcp /t REG_DWORD /d 1 /f
+
