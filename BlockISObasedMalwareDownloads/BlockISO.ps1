ebab67dc8f5459d33ae0d63b0875377f8a9f170f
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 07:21:41 +0000

    Create BlockISO.ps1

---
+++ BlockISObasedMalwareDownloads/BlockISO.ps1
@@ -0,0 +1,2 @@
+reg add "HKEY_CLASSES_ROOT\Windows.IsoFile\shell\mount" /v ProgrammaticAccessOnly /t REG_SZ /f
+reg add "HKEY_CLASSES_ROOT\Windows.VhdFile\shell\mount" /v ProgrammaticAccessOnly /t REG_SZ /f
