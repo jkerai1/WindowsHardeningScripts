a7e7d6014ff68be7e75d0125e296ecfe53317a2a
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-20 06:56:02 +0000

    Update DisableEnumOnElevation.ps1

--- DisableEnumOnElevation/DisableEnumOnElevation.ps1
+++ DisableEnumOnElevation/DisableEnumOnElevation.ps1
@@ -4,7 +4,7 @@ $key0 = "EnumerateAdministrators"
 
 # Check if the value exists and is already set to 0
 if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key0 -ErrorAction SilentlyContinue).$key0 -eq 0) {
-    Write-Host "The value of '$key0' is already set to 0."
+    Write-Host "The value of '$key0' under $regKey is already set to 0."
 
 }
 else {
@@ -12,7 +12,7 @@ try {
     # Set the value
     New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\CredUI"
     Set-ItemProperty -Path $regKey -Name $key0 -Value 0 -Type DWORD -Force
-    Write-Host "The value of '$key0' has been set to 0."
+    Write-Host "The value of '$key0' under $regKey has been set to 0."
 }
 
 catch {
