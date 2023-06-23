49062d4180677b692a5cf9b8b8de052c8aeaa2d0
J/ <55988027+jkerai1@users.noreply.github.com>
2023-06-23 08:03:11 +0000

    Create DisableWifiSense.ps1

---
+++ DisableWifiSense/DisableWifiSense.ps1
@@ -0,0 +1,5 @@
+If (!(Test-Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
+	New-Item -Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Name "Value" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Name "Value" -Type DWord -Value 0
