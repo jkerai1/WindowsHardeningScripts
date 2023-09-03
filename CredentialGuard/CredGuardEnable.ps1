e6fdd38782ea594f4902494408401dbe3191633c
J/ <55988027+jkerai1@users.noreply.github.com>
2023-09-03 11:42:10 +0000

    Update CredGuardEnable.ps1

--- CredentialGuard/CredGuardEnable.ps1
+++ CredentialGuard/CredGuardEnable.ps1
@@ -6,15 +6,15 @@ $key2 = "RequirePlatformSecurityFeatures"
 $key3 = "EnableVirtualizationBasedSecurity"
 
 # Check if the value exists and is already set to 1
-if ((Test-Path $regKey) -and (Get-ItemProperty $regKey2 -Name $key1 -ErrorAction SilentlyContinue).$key1 -eq 1) {
-    Write-Host "The value of '$key2' under $regKey2 is already set to 1."
+if ((Test-Path $regKey2) -and (Get-ItemProperty $regKey2 -Name $key3 -ErrorAction SilentlyContinue).$key3 -eq 1) { #HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard & EnableVirtualizationBasedSecurity
+    Write-Host "The value of '$key3' under $regKey2 is already set to 1."
 
 }
 else {
 try {
     # Set the value to 1
-    Set-ItemProperty -Path $regKey2 -Name $key1 -Value 1 -Type DWORD -Force
-    Write-Host "The value of '$key2' under $regKey2 has been set to 1."
+    Set-ItemProperty -Path $regKey2 -Name $key3 -Value 1 -Type DWORD -Force
+    Write-Host "The value of '$key3' under $regKey2 has been set to 1."
 }
 
 catch {
@@ -23,15 +23,15 @@ catch {
 }
 
 # Check if the value exists and is already set to 3
-if ((Test-Path $regKey) -and (Get-ItemProperty $regKey2 -Name $key2 -ErrorAction SilentlyContinue).$key1 -eq 3) {
-    Write-Host "The value of '$key3' under $regKey2 is already set to 3."
+if ((Test-Path $regKey2) -and (Get-ItemProperty $regKey2 -Name $key2 -ErrorAction SilentlyContinue).$key2 -eq 3) {
+    Write-Host "The value of '$key2' under $regKey2 is already set to 3."
 
 }
 else {
 try {
     # Set the value to 1
-    Set-ItemProperty -Path $regKey2 -Name $key1 -Value 3 -Type DWORD -Force
-    Write-Host "The value of '$key3' under $regKey2 has been set to 3." ##SecureBoot & DMA Protection
+    Set-ItemProperty -Path $regKey2 -Name $key2 -Value 3 -Type DWORD -Force #RequirePlatformSecurityFeatures under HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard
+    Write-Host "The value of '$key2' under $regKey2 has been set to 3." ##SecureBoot & DMA Protection
 }
 
 catch {
@@ -48,7 +48,7 @@ if ((Test-Path $regKey) -and (Get-ItemProperty $regKey -Name $key1 -ErrorAction 
 else {
 try {
     # Set the value to 1
-    Set-ItemProperty -Path $regKey -Name $key1 -Value 1 -Type DWORD -Force
+    Set-ItemProperty -Path $regKey -Name $key1 -Value 1 -Type DWORD -Force # "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" & "LsaCfgFlags"
     Write-Host "The value of '$key1' under $regKey has been set to 1." #Windows Defender Credential Guard with UEFI lock
 }
 
