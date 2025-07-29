8f1f911234e5dadc7048e30fab45159375ef2f87
J/ <55988027+jkerai1@users.noreply.github.com>
2025-07-29 22:17:20 +0000

    Update RequireAddtionalAuthenticationAtStartup.ps1

--- RequireAdditionalAuthenticationAtStartup/RequireAddtionalAuthenticationAtStartup.ps1
+++ RequireAdditionalAuthenticationAtStartup/RequireAddtionalAuthenticationAtStartup.ps1
@@ -1,1 +1,2 @@
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" ##if doesnt exist then create it - may fail but script should continue
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "UseAdvancedStartup"  -Value "1"
